7fb36a933a03bf5ceb536a3785c2e10430d6ad65
null <null>
2025-09-08 13:31:15 +0530

    Test

--- src/Components/AppRat.js
+++ src/Components/AppRat.js
@@ -1,7 +1,35 @@
-import React from 'react';
-import '../style.css';
+import { Grid, Paper, Typography } from '@mui/material';
+
 const AppRat = () => {
-    return(<div>AppRat</div>)
-}
+	return (
+		<Grid item xs={12}>
+			<Paper sx={{ p: 2 }}>
+				<Typography variant='h6' gutterBottom sx={{ color: '#00ffff' }}>
+					App Rat
+				</Typography>
+				<Grid container spacing={1}>
+					<Grid item xs={12}>
+						<Typography variant='h5' color='success.main'>
+							220K
+						</Typography>
+						<Typography variant='body2'>Dollars savings</Typography>
+					</Grid>
+					<Grid item xs={12}>
+						<Typography variant='h5' color='success.main'>
+							120k
+						</Typography>
+						<Typography variant='body2'>SAP Mobile Platform</Typography>
+					</Grid>
+					<Grid item xs={12}>
+						<Typography variant='h5' color='success.main'>
+							100k
+						</Typography>
+						<Typography variant='body2'>SAP CE</Typography>
+					</Grid>
+				</Grid>
+			</Paper>
+		</Grid>
+	);
+};
 
 export default AppRat;
