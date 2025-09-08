ef5d4cb171296d98da0ed2722bf37bc9a5944224
arunjoseexpedia <k.arunjose@gmail.com>
2025-09-08 12:28:42 +0530

    test2

---
+++ src/Components/AppRat.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../style.css';
+const AppRat = () => {
+    return(<div>AppRat</div>)
+}
+
+export default AppRat;
---
+++ src/Components/TechDebtReduction.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../style.css';
+const TechDebtReduction = () => {
+    return(<div>TechDebt Reduction 25%</div>)
+}
+
+export default TechDebtReduction;
